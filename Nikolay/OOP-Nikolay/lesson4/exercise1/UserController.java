9d81410b8c55b6a919cecf1e5a95036701d88ae6
NikolaySad <146080859+NikolaySad@users.noreply.github.com>
2024-11-14 15:26:58 +0300

    Update UserController.java

--- Nikolay/OOP-Nikolay/lesson4/exercise1/UserController.java
+++ Nikolay/OOP-Nikolay/lesson4/exercise1/UserController.java
@@ -1,4 +1,4 @@
-package OOP-Nikolay.lesson4.exercise1;
+package Nikolay.OOP-Nikolay.lesson4.exercise1;
 
 public interface UserController<T extends User> {
 	void create(String surname, String firstname, String patronymic);
